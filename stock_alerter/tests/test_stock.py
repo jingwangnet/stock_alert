2b86bf4d3f1a17376eeef7ea9f9bf4b6b1544f51
jing wang <admin@jingwangnet.com>
2022-08-12 23:07:52 -0400

    REFCOTRY Using setUp method to instead of repeating this setup code in each and every test.

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -4,30 +4,29 @@ from ..stock import Stock
 
 
 class StockTest(unittest.TestCase):
+
+    def setUp(self):
+        self.goog = Stock('GOOG')
     
     def test_price_of_a_stock_class_should_be_None(self):
-        stock = Stock('GOOG')
-        self.assertIsNone(stock.price)
+        self.assertIsNone(self.goog.price)
 
     def test_stock_update(self):
         """An update shout set the price on thestock object
         We will be using the `datetime` module for timestamp
         """
 
-        goog = Stock('GOOG')
-        goog.update(datetime(2014, 2, 12), price=10)
-        self.assertEqual(10, goog.price)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.assertEqual(10, self.goog.price)
 
     def test_negative_price_shout_throw_ValueError(self):
-        goog = Stock('GOOG')
         with self.assertRaises(ValueError): 
-            goog.update(datetime(2014, 2, 13), -1)
+            self.goog.update(datetime(2014, 2, 13), -1)
 
     def test_stock_price_should_gice_the_latest_price(self):
-        goog = Stock('GOOG')
-        goog.update(datetime(2014, 2, 12), price=10)
-        goog.update(datetime(2014, 2, 13), price=8.4)
-        self.assertAlmostEqual(8.4, goog.price, delta=0.0001)
+        self.goog.update(datetime(2014, 2, 12), price=10)
+        self.goog.update(datetime(2014, 2, 13), price=8.4)
+        self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
         
 
 
