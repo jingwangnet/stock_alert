edac32719379289d9f7cb4a1e8cfdd9c4ef49806
jing wang <admin@jingwangnet.com>
2022-08-13 01:23:45 -0400

    GREEN Add is_increasing_trend method.

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -7,12 +7,18 @@ class Stock:
         self.symbol = symbol
         self.price_history = []
 
+    @property
+    def price(self):
+        return self.price_history[-1] \
+            if self.price_history else None
+
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("Price shout not be negative")
         self.price_history.append(price)
 
-    @property
-    def price(self):
-        return self.price_history[-1] if self.price_history else None
+    def is_increasing_trend(self):
+        return self.price_history[-3] < \
+            self.price_history[-2] < self.price_history[-1]
+
 
