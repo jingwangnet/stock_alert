f29ca3fbaf8f7726fd3f175f5d7d5dc32126f1b0
jing wang <admin@jingwangnet.com>
2022-08-13 02:06:48 -0400

    Refacotry Adding a help method, given_a_serise_of_prices to remove dumplicate code.

--- stock_alerter/tests/test_stock.py
+++ stock_alerter/tests/test_stock.py
@@ -23,31 +23,27 @@ class StockTest(unittest.TestCase):
         with self.assertRaises(ValueError): 
             self.goog.update(datetime(2014, 2, 13), -1)
 
-    def test_stock_price_should_gice_the_latest_price(self):
+    def test_stock_price_should_give_the_latest_price(self):
         self.goog.update(datetime(2014, 2, 12), price=10)
         self.goog.update(datetime(2014, 2, 13), price=8.4)
         self.assertAlmostEqual(8.4, self.goog.price, delta=0.0001)
 
-    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+    def given_a_series_of_prices(self, prices):
         timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12), datetime(2014, 2, 13)]
-        prices = [8, 10, 12]
         for timestamp, price in zip(timestamps, prices):
             self.goog.update(timestamp, price)
+
+    def test_increasing_trend_is_true_if_price_increase_for_3_updates(self):
+        self.given_a_series_of_prices([8, 10, 12])
         self.assertTrue(self.goog.is_increasing_trend())
         
 
     def test_increasting_trend_is_false_if_price_decreas(self):
-        timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12), datetime(2014, 2, 13)]
-        prices = [8,  12, 10]
-        for timestamp, price in zip(timestamps, prices):
-            self.goog.update(timestamp, price)
+        self.given_a_series_of_prices([12, 10, 8])
         self.assertFalse(self.goog.is_increasing_trend())
 
     def test_increasing_trend_is_false_if_price_equal(self):
-        timestamps = [datetime(2014, 2, 11), datetime(2014, 2, 12), datetime(2014, 2, 13)]
-        prices = [8,  10, 10]
-        for timestamp, price in zip(timestamps, prices):
-            self.goog.update(timestamp, price)
+        self.given_a_series_of_prices([8, 10, 10])
         self.assertFalse(self.goog.is_increasing_trend())
 
 
