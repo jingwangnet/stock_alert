1931fc62604fec853e87bfef93571180b72f0b0a
jing wang <admin@jingwangnet.com>
2022-08-13 22:57:38 -0400

    REFACTORY Use collections.namedtuple make code clear.

--- stock_alerter/stock.py
+++ stock_alerter/stock.py
@@ -1,5 +1,11 @@
 import unittest
 import bisect
+import collections
+
+
+PriceEvent = collections.namedtuple('PriceEvent',
+    ['timestamp', 'price'])
+
 
 
 class Stock:
@@ -10,17 +16,17 @@ class Stock:
 
     @property
     def price(self):
-        return self.price_history[-1][1] \
+        return self.price_history[-1].price \
             if self.price_history else None
 
     def update(self, timestamp, price):
         if price < 0:
             raise ValueError("Price shout not be negative")
         #self.price_history.append(price)
-        bisect.insort_left(self.price_history, (timestamp, price))
+        bisect.insort_left(self.price_history, PriceEvent(timestamp, price))
 
     def is_increasing_trend(self):
-        return self.price_history[-3][1] < \
-            self.price_history[-2][1] < self.price_history[-1][1]
+        return self.price_history[-3].price < \
+            self.price_history[-2].price < self.price_history[-1].price
 
 
